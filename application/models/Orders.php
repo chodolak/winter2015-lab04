7427c169cbac04608cd7de0d3f1617f7ed28c9c2
Michael Chodolak <chodolak@gmail.com>
2015-02-10 14:50:54 -0800

    Updated order model with validation. Signed-off-by:Michael Chodolak <chodolak@gmail.com>

--- application/models/Orders.php
+++ application/models/Orders.php
@@ -14,16 +14,38 @@ class Orders extends MY_Model {
 
     // add an item to an order
     function add_item($num, $code) {
-        if($this->orders->excist('code', $code))
+        //If the list isn't empty then it adds it to the list
+        //else it creates a new list
+        if (($item = $this->orderitems->get($num, $code)) != null)
         {
+            $item->quantity += 1;
+            $this->orderitems->update($item);
+        }
+        else
+        {
+            $item = $this->orderitems->create();
+            $item->order = $num;
+            $item->item = $code;
+            $item->quantity = 1;
             
-            
+            $this->orderitems->add($item);
         }
     }
 
     // calculate the total for an order
     function total($num) {
-        return 0.0;
+        //Gets the order
+        $orderitems = $this->orderitems->some('order', $num);
+        
+        $total = 0.0;
+        
+        //Goes through the entire order adding up the total
+        foreach($orderitems as $item)
+        {
+            $total += $item->quantity * $this->menu->get($item->item)->price;
+        }
+        
+        return $total;
     }
 
     // retrieve the details for an order
@@ -39,7 +61,24 @@ class Orders extends MY_Model {
     // validate an order
     // it must have at least one item from each category
     function validate($num) {
-        return false;
+        $catagories = array();
+        $items = $this->orderitems->group($num);
+        
+        
+        //Goes through all items and checks the catagory.
+        //Sets the catagory to 1 if it has it.
+        if (count($items) > 0)
+        {
+            foreach ($items as $item)
+            {
+                $menu = $this->menu->get($item->item);
+                $catagories[$menu->category] = 1;
+            }
+        }
+        
+        //Returns true if all the catagories have a 1
+        //else it returns false.
+        return isset($catagories['m']) && isset($catagories['d']) && isset($catagories['s']);
     }
 
 }
